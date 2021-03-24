253e9b50be027dbd314f70529afa47ed2d915078
Holly Parsons <67122062+hmparsons-sde@users.noreply.github.com>
2021-03-23 20:43:45 -0500

    fully functional

--- src/javascripts/components/jokePage.js
+++ src/javascripts/components/jokePage.js
@@ -1,5 +1,5 @@
 import joke from '../../assets/joke.png';
-import { givePunchline, giveSetup } from '../helpers/jokes';
+// import { givePunchline, giveSetup } from '../helpers/jokes';
 
 const jokeContainer = () => {
   document.querySelector('#joke-container').innerHTML = `
@@ -13,24 +13,24 @@ const jokeContainer = () => {
   </div>`;
 };
 
-const showJoke = () => {
+const showJoke = (jokeObject) => {
   document.querySelector('#joke-container').innerHTML = `
   <div class="container" id="jokeContainer">
     <div class="title">
       <img src="${joke}" class="img-fluid" alt="Responsive image">
     </div>
-    <div class="jokeDisplay">${giveSetup()}</div>
+    <div class="jokeDisplay">${jokeObject.setup}</div>
     <button type="button" class="btn btn-dark jokeButton" id="getPunchLine">GET PUNCHLINE</button>
   </div>`;
 };
 
-const showPunchline = () => {
+const showPunchline = (jokeObject) => {
   document.querySelector('#joke-container').innerHTML = `
   <div class="container" id="jokeContainer">
     <div class="title">
       <img src="${joke}" class="img-fluid" alt="Responsive image">
     </div>
-    <div class="punchLine">${givePunchline()}</div>
+    <div class="punchLine">${jokeObject.punchline}</div>
     <button type="button" class="btn btn-dark jokeButton" id="getNewJoke">GET A NEW JOKE</button>
   </div>`;
 };
